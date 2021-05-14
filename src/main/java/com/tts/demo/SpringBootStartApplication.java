236b9f4625bc64d8b2e93fb710f3bc8db5048eeb
katemejaski <katmackjj@gmail.com>
2021-05-14 11:26:13 -0400

    Initial commit

---
+++ src/main/java/com/tts/demo/SpringBootStartApplication.java
@@ -0,0 +1,14 @@
+package com.tts.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+    @SpringBootApplication
+    public class SpringBootStartApplication {
+
+        public static void main(String[] args) {
+            SpringApplication.run(Application.class, args);
+        }
+
+    }
+}
